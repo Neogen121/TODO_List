058d89103c305b0df37d703eb2d9123351f2f66b
Neogen121 <mkasatka@gmail.com>
2022-03-14 21:01:14 +0300

    ver02

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
         <div class="header">
             <div class="header__title" @dblclick="setDefaultTasks()">TO-DO List</div>
             <div class="header__controls">
-                <img class="header__add" src="./assets/ui/add.svg" @click="addTask()" />
+                <img class="header__control" src="./assets/ui/info.svg" @click="showInfo" />
+                <img class="header__control" src="./assets/ui/add.svg" @click="addTask" />
                 <img
-                    class="header__remove"
-                    :class="selectedIndexes.length === 0 ? 'header__remove--hidden' : ''"
+                    class="header__control"
+                    :class="selectedIndexes.length === 0 ? 'header__control--hidden' : ''"
                     src="./assets/ui/remove.svg"
                     @click="removeTasks()"
                 />
@@ -53,11 +54,23 @@ export default {
         },
 
         removeTasks() {
-            for (let i = this.selectedIndexes.length - 1; i >= 0; i--) {
-                this.tasks.splice(this.selectedIndexes[i], 1);
+            if (confirm("Do you really want to delete?")) {
+                for (let i = this.selectedIndexes.length - 1; i >= 0; i--) {
+                    this.tasks.splice(this.selectedIndexes[i], 1);
+                }
+                this.selectedIndexes = [];
+                this.assignIDs();
             }
-            this.selectedIndexes = [];
-            this.assignIDs();
+        },
+
+        showInfo() {
+            const info = `
+            doubleclick on text field to edit
+            long click for multiselection
+            drag right side to reorder
+            doubleclick header title to reset
+            `;
+            alert(info);
         },
 
         taskChange(data) {
